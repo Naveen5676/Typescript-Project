041d8f6707d2c5ecf8bad3c9f147700b57e128ab
Naveen5676 <sample.first@yahoo.com>
2024-05-01 09:57:39 +0530

    using type casting

--- routes/todos.js
+++ routes/todos.js
@@ -7,23 +7,27 @@ router.get('/todoget', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todopost', (req, res, next) => {
+    const body = req.body;
     const newtodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newtodo);
     res.status(201).json(newtodo);
 });
 router.post('/tododelete', (req, res, next) => {
-    const deleteid = req.body.id;
+    const body = req.body;
+    const deleteid = body.id;
     todos = todos.filter((data) => (data.id != deleteid));
     res.status(200).json(todos);
 });
 router.put('/todo/:todoid', (req, res, next) => {
-    const todoid = req.params.todoid;
+    const params = req.params;
+    const todoid = params.todoid;
+    const body = req.body;
     const todoindex = todos.findIndex(d => d.id === todoid);
     if (todoindex >= 0) {
-        todos[todoindex] = { id: todoid, text: req.body.text };
+        todos[todoindex] = { id: todoid, text: body.text };
         return res.status(100).json(todos);
     }
     res.status(404).json({ message: "could not find todo for this id" });
--- routes/todos.ts
+++ routes/todos.ts
@@ -2,6 +2,9 @@ import {Router} from 'express'
 import { Todo } from '../models/todo'
 import { todo } from 'node:test';
 
+type requestBody =  {text : string};
+type RequestParams = { todoid : string}
+
 let todos: Todo[]=[]
 
 const router = Router();
@@ -10,25 +13,29 @@ router.get('/todoget', (req,res,next)=>{
   res.status(200).json({todos: todos})
 })
 router.post('/todopost' , (req,res,next)=>{
+    const body = req.body as requestBody
     const newtodo: Todo = {
         id:new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newtodo);
     res.status(201).json(newtodo)
 })
 
 router.post('/tododelete', (req,res,next)=>{
-  const deleteid = req.body.id;
+   const body = req.body as {id: string}
+  const deleteid = body.id;
   todos = todos.filter((data)=>( data.id != deleteid))
   res.status(200).json(todos)
 })
 
 router.put('/todo/:todoid', (req,res,next)=>{
-  const todoid = req.params.todoid;
+  const params = req.params as RequestParams
+  const todoid = params.todoid;
+  const body = req.body as requestBody
   const todoindex = todos.findIndex(d => d.id === todoid);
   if(todoindex >= 0){
-    todos[todoindex]= { id : todoid, text : req.body.text}
+    todos[todoindex]= { id : todoid, text : body.text}
     return  res.status(100).json(todos)
   }
   res.status(404).json({message : "could not find todo for this id"})
